96abf67e46cd2fdfeba06cfd3b2da05cf8f0f23e
熊鸣 <625225813@qq.com>
2020-07-31 16:38:05 +0800

    changshi

---
+++ src/one.java
@@ -0,0 +1,7 @@
+public class one {
+    public static void main(String[] args) {
+
+        System.out.println(1);
+        System.out.println(2);
+    }
+}
---
+++ src/three.java
@@ -0,0 +1,5 @@
+public class three {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
